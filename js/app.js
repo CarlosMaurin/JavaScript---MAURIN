307ad0e42fe599b02a86093ca80a00d81741ae68
CarlosMaurin <93627645+CarlosMaurin@users.noreply.github.com>
2022-02-11 11:07:41 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,6 @@
-//para saltar lineas, usar document.write(+"<br>"+)//
-//para poder usar ${} tengo q colocar `
-
 alert("Bienvenido al BREW-LAB. A continuacion crearemos tu receta");
 
-let densidadInicial;
+
 let litros;
 let galones;
 
@@ -25,104 +22,66 @@ galones = litros * 0.264172;
 
 document.write(`Usted tiene pensado producir ${litros} de cerveza <br>`);
 
-
-
 alert("Ahora seleccionaremos las maltas a utilizar");
 
 
-function Fermentable (nombre, extracto, color, utiliza, kilos, libras, densidad){
-    this.nombre = nombre;
-    this.extracto = extracto;
-    this.color = color;
-    this.utiliza = utiliza;
-    this.kilos = kilos;
-    this.libras = libras;
-    this.densidad = densidad;
-    this.calculoDensidad = function (){
+class Granos{
+    constructor(nombre, extracto, color, utiliza, kilos, libras, densidad, mcu){
+        this.nombre = nombre;
+        this.extracto = extracto;
+        this.color = color;
+        this.utiliza = utiliza;
+        this.kilos = kilos;
+        this.libras = libras;
+        this.densidad = densidad;
+        this.mcu = mcu
+    }
+    calculoDensidad(){
         if(this.utiliza == "SI"){
             this.densidad = this.libras * this.extracto * eficiencia;
         }
     }
 }
 
-let pilsen = new Fermentable ("pilsen", 37, 2, true, 0, 0, 0);
-let cara30 = new Fermentable ("cara30", 35, 30, true, 0, 0, 0);
-let cara60 = new Fermentable ("cara60", 34, 60, true, 0, 0, 0);
-let cara120 = new Fermentable ("cara120", 33, 120, true, 0, 0, 0);
-let chocolate = new Fermentable ("chocolate", 29, 400, true, 0, 0, 0);
+const fermentables = []; 
 
+fermentables.push(new Granos ("pilsen", 37, 2, false, 0, 0, 0, 0));
+fermentables.push(new Granos ("cara30", 35, 30, false, 0, 0, 0, 0));
+fermentables.push(new Granos ("cara60", 34, 60, false, 0, 0, 0, 0));
+fermentables.push(new Granos ("cara120", 33, 120, false, 0, 0, 0, 0));
+fermentables.push(new Granos ("chocolate", 29, 400, false, 0, 0, 0, 0));
 
-pilsen.utiliza = prompt("te gustaría usar malta Pilsen? Ingrese SI en caso afirmativo. Ingrese NO en caso negativo").toUpperCase();
-cara30.utiliza = prompt("te gustaría usar malta Cara30? Ingrese SI en caso afirmativo. Ingrese NO en caso negativo").toUpperCase();
-cara60.utiliza = prompt("te gustaría usar malta Cara60? Ingrese SI en caso afirmativo. Ingrese NO en caso negativo").toUpperCase();
-cara120.utiliza = prompt("te gustaría usar malta Cara120? Ingrese SI en caso afirmativo. Ingrese NO en caso negativo").toUpperCase();
-chocolate.utiliza = prompt("te gustaría usar malta Chocolate? Ingrese SI en caso afirmativo. Ingrese NO en caso negativo").toUpperCase();
-
-
-if(pilsen.utiliza == "SI"){
-    pilsen.kilos = parseInt(prompt("Cuantos kilos de malta pilsen deseas usar?"));
-    pilsen.libras = pilsen.kilos / 0.453592;
-}
-if(cara30.utiliza == "SI"){
-    cara30.kilos = parseInt(prompt("Cuantos kilos de malta cara30 deseas usar?"));
-    cara30.libras = cara30.kilos / 0.453592;
-}
-if(cara60.utiliza == "SI"){
-    cara60.kilos = parseInt(prompt("Cuantos kilos de malta cara60 deseas usar?"));
-    cara60.libras = cara60.kilos / 0.453592;
-}
-if(cara120.utiliza == "SI"){
-    cara120.kilos = parseInt(prompt("Cuantos kilos de malta cara120 deseas usar?"));
-    cara120.libras = cara120.kilos / 0.453592;
-}
-if(chocolate.utiliza == "SI"){
-    chocolate.kilos = parseInt(prompt("Cuantos kilos de malta chocolate deseas usar?"));
-    chocolate.libras = chocolate.kilos / 0.453592;
+for(let i=0; i<fermentables.length; i+=1){
+    fermentables[i].utiliza = prompt(`te gustaría usar malta ${fermentables[i].nombre}? Ingrese SI en caso afirmativo. Ingrese NO en caso negativo`).toUpperCase();
 }
 
-document.write(`Ud utilizará en su receta la siguiente lista de maltas`);
+for(let i=0; i<fermentables.length; i+=1){
+    if(fermentables[i].utiliza == "SI"){
+        fermentables[i].kilos = parseInt(prompt(`Cuantos kilos de malta ${fermentables[i].nombre} deseas usar?`));
+        fermentables[i].libras = fermentables[i].kilos / 0.453592;
+        
+    }
+}
+
 document.write("<br> <br>");
-if(pilsen.kilos > 0 ){
-    document.write(`${pilsen.kilos} kilos de malta ${pilsen.nombre} <br>`);
-}
-if(cara30.kilos > 0 ){
-    document.write(`${cara30.kilos} kilos de malta ${cara30.nombre} <br>`);
-}
-if(cara60.kilos > 0 ){
-    document.write(`${cara60.kilos} kilos de malta ${cara60.nombre} <br>`);
-}
-if(cara120.kilos > 0 ){
-    document.write(`${cara120.kilos} kilos de malta ${cara120.nombre} <br>`);
-}
-if(chocolate.kilos > 0 ){
-    document.write(`${chocolate.kilos} kilos de malta ${chocolate.nombre} <br>`);
+document.write(`Ud utilizará en su receta la siguiente lista de maltas`);
+document.write("<br> <br>");    
+
+for(let i=0; i<fermentables.length; i+=1){
+    if(fermentables[i].kilos > 0){
+        document.write(`${fermentables[i].kilos} kilos de malta ${fermentables[i].nombre} <br>`);
+        fermentables[i].calculoDensidad();
+    }
 }
 
 
-pilsen.calculoDensidad();
-cara30.calculoDensidad();
-cara60.calculoDensidad();
-cara120.calculoDensidad();
-chocolate.calculoDensidad();
+const densidadInicial = (fermentables.map(item => item.densidad).reduce((prev, curr) => prev + curr, 0)) / galones + 1000;
 
 
-function calculoDensidadInicial (){
-    densidadInicial = (pilsen.densidad + cara30.densidad + cara60.densidad + cara120.densidad + chocolate.densidad) / galones + 1000;
-    return densidadInicial;
-}
-
-alert(`La densidad inicial de tu cerveza es igual a ${Math.round(calculoDensidadInicial())}`);
+alert(`La densidad inicial de tu cerveza es igual a ${Math.round(densidadInicial)}`);
 document.write("<br>");
-document.write(`La densidad inicial de tu cerveza es igual a ${Math.round(calculoDensidadInicial())}`);
+document.write(`La densidad inicial de tu cerveza es igual a ${Math.round(densidadInicial)}`);
 document.write("<hr>");
-
-
-
-// let repetir;
-// do{
-//     calculoMaltas();
-//     repetir = prompt("Para continuar, ingrese SI. Para repetir el proceso, ingrese NO");
-// }while(repetir == "NO");
 
 
 //---------------------CALCULOS DE LUPULO---------------------------////
@@ -130,7 +89,7 @@ document.write("<hr>");
 alert("A continuación calcularemos los IBUs de nuestra receta");
 
 let factorCorreccion = 7.489;
-let ibuTotal;
+
 let correccionDensidad;
 
 if(densidadInicial <= 1050){
@@ -140,195 +99,101 @@ if(densidadInicial <= 1050){
 }
 
 
+// function Lupulo(nombre, AA, gramos, onzas, tiempo, ibu, utiliza){
+//     this.nombre = nombre;
+//     this.AA = AA;
+//     this.gramos = gramos;
+//     this.onzas = onzas;
+//     this.tiempo = tiempo;
+//     this.ibu = ibu;
+//     this.utiliza = utiliza;
+//     this.calculoIbu = function(){
+//         this.ibu = ((this.onzas * this.tiempo * this.AA * factorCorreccion) / (galones*correccionDensidad))*1000;
+//     }
+// }
 
-function Lupulo(nombre, AA, gramos, onzas, tiempo, ibu, utiliza){
-    this.nombre = nombre;
-    this.AA = AA;
-    this.gramos = gramos;
-    this.onzas = onzas;
-    this.tiempo = tiempo;
-    this.ibu = ibu;
-    this.utiliza = utiliza;
-    this.calculoIbu = function(){
-        this.ibu = ((this.onzas * this.tiempo * this.AA * factorCorreccion) / (galones*correccionDensidad))*1000;
+
+class Lupulo{
+    constructor (nombre, AA, gramos, onzas, tiempo, ibu, utiliza){
+        this.nombre = nombre;
+        this.AA = AA;
+        this.gramos = gramos;
+        this.onzas = onzas;
+        this.tiempo = tiempo;
+        this.ibu = ibu;
+        this.utiliza = utiliza;
+    }
+    calculoIbu (){
+            this.ibu = ((this.onzas * this.tiempo * this.AA * factorCorreccion) / (galones*correccionDensidad))*1000;
+        }
+    
+}
+
+let arrLupulo = [];
+
+arrLupulo.push(new Lupulo("cascade", 0.08, 0, 0, 0, 0, false));
+arrLupulo.push(new Lupulo("Kent goldings", 0.05, 0, 0, 0, 0, false));
+arrLupulo.push(new Lupulo("Bravo", 0.12, 0, 0, 0, 0, false));
+arrLupulo.push(new Lupulo("Ekuanot", 0.11, 0, 0, 0, 0, false));
+arrLupulo.push(new Lupulo("Simcoe", 0.11, 0, 0, 0, 0, false));
+
+
+for(i=0; i<arrLupulo.length; i+=1){
+    arrLupulo[i].utiliza = prompt(`desea utilizar lúpulo ${arrLupulo[i].nombre} en su receta? Ingrese SI en caso afirmativo. Ingrese NO en caso negativo`).toUpperCase();
+}
+
+
+for(i=0; i<arrLupulo.length; i+=1){
+    if(arrLupulo[i].utiliza == "SI"){
+        arrLupulo[i].gramos = prompt(`cuántos gramos de ${arrLupulo[i].nombre} desea utilizar?:`);
+        arrLupulo[i].onzas = arrLupulo[i].gramos * 0.035274;
+        arrLupulo[i].tiempo = parseInt(prompt("Durante cuántos minutos de hervor?"));
     }
 }
 
-const cascade = new Lupulo("cascade", 0.08, 0, 0, 0, 0, true);
-const kentGoldings = new Lupulo("Kent goldings", 0.05, 0, 0, 0, 0, true);
-const bravo = new Lupulo("Bravo", 0.12, 0, 0, 0, 0, true);
-const ekuanot = new Lupulo("Ekuanot", 0.11, 0, 0, 0, 0, true);
-const simcoe = new Lupulo("Simcoe", 0.11, 0, 0, 0, 0, true);
-
-cascade.utiliza = prompt("desea utilizar lúpulo Cascade en su receta? Ingrese SI en caso afirmativo. Ingrese NO en caso negativo").toUpperCase();
-kentGoldings.utiliza = prompt("desea utilizar lúpulo Kent Goldings en su receta? Ingrese SI en caso afirmativo. Ingrese NO en caso negativo").toUpperCase();
-bravo.utiliza = prompt("desea utilizar lúpulo Bravo en su receta? Ingrese SI en caso afirmativo. Ingrese NO en caso negativo").toUpperCase();
-ekuanot.utiliza = prompt("desea utilizar lúpulo Ekuanot en su receta? Ingrese SI en caso afirmativo. Ingrese NO en caso negativo").toUpperCase();
-simcoe.utiliza = prompt("desea utilizar lúpulo Simcoe en su receta? Ingrese SI en caso afirmativo. Ingrese NO en caso negativo").toUpperCase();
-
-
-if(cascade.utiliza == "SI"){
-    cascade.gramos = parseInt(prompt("cuántos gramos de CASCADE desea utilizar?:"));
-    cascade.onzas = cascade.gramos * 0.035274;
-    cascade.tiempo = parseInt(prompt("Durante cuántos minutos de hervor?"));
-}
-
-if((cascade.tiempo >= 0) && (cascade.tiempo <= 9)){
-    cascade.tiempo = 0.06; 
-}else if ((cascade.tiempo >= 10) && (cascade.tiempo <= 19)){
-    cascade.tiempo = 0.15;
-}else if ((cascade.tiempo >= 20) && (cascade.tiempo <= 29)){
-    cascade.tiempo = 0.19;
-}else if ((cascade.tiempo >= 30) && (cascade.tiempo <= 44)){
-    cascade.tiempo = 0.24;
-}else if ((cascade.tiempo >= 45) && (cascade.tiempo <= 59)){
-    cascade.tiempo = 0.27;
-}else if ((cascade.tiempo >= 60) && (cascade.tiempo <= 74)){
-    cascade.tiempo = 0.30;
-}else if (cascade.tiempo >= 75){
-    cascade.tiempo = 0.34;
-}
-
-if(kentGoldings.utiliza == "SI"){
-    kentGoldings.gramos = parseInt(prompt("cuántos gramos de KENT GOLDINGS desea utilizar?:"));
-    kentGoldings.onzas = kentGoldings.gramos * 0.035274;
-    kentGoldings.tiempo = parseInt(prompt("Durante cuántos minutos de hervor?"));
-}
+for(i=0; i<arrLupulo.length; i+=1){
+    if((arrLupulo[i].tiempo >= 0) && (arrLupulo[i].tiempo <= 9)){
+        arrLupulo[i].tiempo = 0.06; 
+    }else if ((arrLupulo[i].tiempo >= 10) && (arrLupulo[i].tiempo <= 19)){
+        arrLupulo[i].tiempo = 0.15;
+    }else if ((arrLupulo[i].tiempo >= 20) && (arrLupulo[i].tiempo <= 29)){
+        arrLupulo[i].tiempo = 0.19;
+    }else if ((arrLupulo[i].tiempo >= 30) && (arrLupulo[i].tiempo <= 44)){
+        arrLupulo[i].tiempo = 0.24;
+    }else if ((arrLupulo[i].tiempo >= 45) && (arrLupulo[i].tiempo <= 59)){
+        arrLupulo[i].tiempo = 0.27;
+    }else if ((arrLupulo[i].tiempo >= 60) && (arrLupulo[i].tiempo <= 74)){
+        arrLupulo[i].tiempo = 0.30;
+    }else if (arrLupulo[i].tiempo >= 75){
+        arrLupulo[i].tiempo = 0.34;
+    }
     
-
-if((kentGoldings.tiempo >= 0) && (kentGoldings.tiempo <= 9)){
-    kentGoldings.tiempo = 0.06; 
-}else if ((kentGoldings.tiempo >= 10) && (kentGoldings.tiempo <= 19)){
-    kentGoldings.tiempo = 0.15;
-}else if ((kentGoldings.tiempo >= 20) && (kentGoldings.tiempo <= 29)){
-    kentGoldings.tiempo = 0.19;
-}else if ((kentGoldings.tiempo >= 30) && (kentGoldings.tiempo <= 44)){
-    kentGoldings.tiempo = 0.24;
-}else if ((kentGoldings.tiempo >= 45) && (kentGoldings.tiempo <= 59)){
-    kentGoldings.tiempo = 0.27;
-}else if ((kentGoldings.tiempo >= 60) && (kentGoldings.tiempo <= 74)){
-    kentGoldings.tiempo = 0.30;
-}else if (kentGoldings.tiempo >= 75){
-    kentGoldings.tiempo = 0.34;
 }
 
-if(bravo.utiliza == "SI"){
-    bravo.gramos = parseInt(prompt("cuántos gramos de BRAVO desea utilizar?:"));
-    bravo.onzas = bravo.gramos * 0.035274;
-    bravo.tiempo = parseInt(prompt("Durante cuántos minutos de hervor?"));
+for(i=0; i<arrLupulo.length; i+=1){
+    arrLupulo[i].calculoIbu();
 }
 
-if((bravo.tiempo >= 0) && (bravo.tiempo <= 9)){
-    bravo.tiempo = 0.06; 
-}else if ((bravo.tiempo >= 10) && (bravo.tiempo <= 19)){
-    bravo.tiempo = 0.15;
-}else if ((bravo.tiempo >= 20) && (bravo.tiempo <= 29)){
-    bravo.tiempo = 0.19;
-}else if ((bravo.tiempo >= 30) && (bravo.tiempo <= 44)){
-    bravo.tiempo = 0.24;
-}else if ((bravo.tiempo >= 45) && (bravo.tiempo <= 59)){
-    bravo.tiempo = 0.27;
-}else if ((bravo.tiempo >= 60) && (bravo.tiempo <= 74)){
-    bravo.tiempo = 0.30;
-}else if (bravo.tiempo >= 75){
-    bravo.tiempo = 0.34;
-}
+const ibuTotal = (arrLupulo.map(item => item.ibu).reduce((prev, curr) => prev + curr, 0));
 
-if(ekuanot.utiliza == "SI"){
-    ekuanot.gramos = parseInt(prompt("cuántos gramos de EKUANOT desea utilizar?:"));
-    ekuanot.onzas = ekuanot.gramos * 0.035274;
-    ekuanot.tiempo = parseInt(prompt("Durante cuántos minutos de hervor?"));
-
-}
-
-if((ekuanot.tiempo >= 0) && (cascade.tiempo <= 9)){
-    ekuanot.tiempo = 0.06; 
-}else if ((ekuanot.tiempo >= 10) && (ekuanot.tiempo <= 19)){
-    ekuanot.tiempo = 0.15;
-}else if ((ekuanot.tiempo >= 20) && (ekuanot.tiempo <= 29)){
-    ekuanot.tiempo = 0.19;
-}else if ((ekuanot.tiempo >= 30) && (ekuanot.tiempo <= 44)){
-    ekuanot.tiempo = 0.24;
-}else if ((ekuanot.tiempo >= 45) && (ekuanot.tiempo <= 59)){
-    ekuanot.tiempo = 0.27;
-}else if ((ekuanot.tiempo >= 60) && (ekuanot.tiempo <= 74)){
-    ekuanot.tiempo = 0.30;
-}else if (ekuanot.tiempo >= 75){
-    ekuanot.tiempo = 0.34;
-}
-
-if(simcoe.utiliza == "SI"){
-    simcoe.gramos = parseInt(prompt("cuántos gramos de SIMCOE desea utilizar?:"));
-    simcoe.onzas = simcoe.gramos * 0.035274;
-    simcoe.tiempo = parseInt(prompt("Durante cuántos minutos de hervor?"));
-}
-    
-
-if((simcoe.tiempo >= 0) && (simcoe.tiempo <= 9)){
-    simcoe.tiempo = 0.06; 
-}else if ((simcoe.tiempo >= 10) && (simcoe.tiempo <= 19)){
-    simcoe.tiempo = 0.15;
-}else if ((simcoe.tiempo >= 20) && (simcoe.tiempo <= 29)){
-    simcoe.tiempo = 0.19;
-}else if ((simcoe.tiempo >= 30) && (simcoe.tiempo <= 44)){
-    simcoe.tiempo = 0.24;
-}else if ((simcoe.tiempo >= 45) && (simcoe.tiempo <= 59)){
-    simcoe.tiempo = 0.27;
-}else if ((simcoe.tiempo >= 60) && (simcoe.tiempo <= 74)){
-    simcoe.tiempo = 0.30;
-}else if (simcoe.tiempo >= 75){
-    simcoe.tiempo = 0.34;
-}
-
-document.write(`Ud utilizará en su receta la siguiente lista de Lúpulos:`);
-document.write("<br> <br>");
-if(cascade.gramos > 0 ){
-    document.write(`${cascade.gramos} gramos de lúpulo ${cascade.nombre} <br>`);
-}
-if(kentGoldings.gramos > 0 ){
-    document.write(`${kentGoldings.gramos} gramos de lúpulo ${kentGoldings.nombre} <br>`);
-}
-if(bravo.gramos > 0 ){
-    document.write(`${bravo.gramos} gramos de lúpulo ${bravo.nombre} <br>`);
-}
-if(ekuanot.gramos > 0 ){
-    document.write(`${ekuanot.gramos} gramos de lúpulo ${ekuanot.nombre} <br>`);
-}
-if(simcoe.gramos > 0 ){
-    document.write(`${simcoe.gramos} gramos de lúpulo ${simcoe.nombre} <br>`);
-}
-
-
-cascade.calculoIbu();
-kentGoldings.calculoIbu();
-bravo.calculoIbu();
-ekuanot.calculoIbu();
-simcoe.calculoIbu();
-
-console.log(cascade);
-console.log(kentGoldings);
-console.log(bravo);
-console.log(ekuanot);
-console.log(simcoe);
-
-
-function calculoIbuTotal(){
-    ibuTotal = cascade.ibu + kentGoldings.ibu + bravo.ibu + ekuanot.ibu + simcoe.ibu;
-    return ibuTotal;
-}
-
-alert(`Los IBUs de tu cerveza son iguales a ${Math.round(calculoIbuTotal())}`);
+alert(`Los IBUs de tu cerveza son iguales a ${Math.round(ibuTotal)}`);
 document.write("<br>");
-document.write(`Los IBUs de tu cerveza son iguales a ${Math.round(calculoIbuTotal())}`);
+document.write(`Los IBUs de tu cerveza son iguales a ${Math.round(ibuTotal)}`);
 document.write("<hr>");
-
 
 
 //--------------------CALCULOS DEL COLOR DE LA CERVEZA----------------////
 
-let mcu = ((pilsen.kilos * pilsen.color + cara30.kilos * cara30.color + cara60.kilos * cara60.color + cara120.kilos * cara120.color + chocolate.kilos * chocolate.color) / litros) * 8.462;
+for(i=0; i<fermentables.length; i+=1){
+   fermentables[i].mcu = fermentables[i].kilos * fermentables[i].color;
+}
 
-let srm = Math.round(1.5 * mcu ** 0.7);
+
+
+const mcuTotal = ((fermentables.map(item => item.mcu).reduce((prev, curr) => prev + curr, 0)) / litros) * 8.462;;
+
+
+let srm = Math.round(1.5 * mcuTotal ** 0.7);
 
 document.write(`El color de su cerveza es de SRM = ${srm}`);
 document.write("<br><br>");
